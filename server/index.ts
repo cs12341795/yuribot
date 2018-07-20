cd674e9523aa71a3cb7bdbe3a950c205c8c4b86f
supmiku39 <supmiku39@gmail.com>
2018-07-20 14:38:55 +0900

    discord.js test

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,17 @@
-import * as Koa from 'koa';
+import Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', ()=>console.log('ready'));
+client.on('message', message =>
+{
+  const {author, content} = message;
+  console.log(`${author.username}: ${content}`);
+});
+client.login(process.env.token);
+
+export default client;
+
+/*import * as Koa from 'koa';
 import config from './config';
 import logger from './logger';
 
@@ -13,4 +26,4 @@ const server = app.listen(config.server.port, config.server.host, () => {
   logger.info(`Server listening on ${config.server.host}:${config.server.port}`);
 });
 
-export default server;
+export default server;*/
