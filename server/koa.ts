803b23eec460307558d864583480ee017d1b6ad0
Rueian Oneecom <rueiancsie@gmail.com>
2018-08-03 01:18:01 +0800

    Limit privilege of some channels to special roles

--- server/koa.ts
+++ server/koa.ts
@@ -24,6 +24,11 @@ interface IYuriKoaOption {
   healthChecker: IYuriKoaHealtherChecker
 }
 
+interface IRole {
+  id: string;
+  name: string;
+}
+
 export default class YuriKoa {
   private opt: IYuriKoaOption;
   private app: Koa;
@@ -63,6 +68,7 @@ export default class YuriKoa {
       const { state, code }: { state: string, code: string} = ctx.query;
       if (!ctx.session || ctx.session.state !== state) {
         ctx.throw(400, 'Bad Oauth state');
+        return;
       }
 
       let token: Oauth2.Token | undefined;
@@ -79,20 +85,16 @@ export default class YuriKoa {
         client.defaults.headers.common['Authorization'] = `${token.token.token_type} ${token.token.access_token}`;
         let user = await client.get('https://discordapp.com/api/users/@me');
         // make sure user is in the guild and get roles at the same time
-        client.defaults.headers.common['Authorization'] = 'Bot ' + config.discord.bot.token;
-        let member = await client.get(`https://discordapp.com/api/guilds/${config.discord.auth.guildId}/members/${user.data.id}`);
-        let roles = await client.get(`https://discordapp.com/api/guilds/${config.discord.auth.guildId}/roles`);
+        let roles = await this.getMemberRoles(user.data.id);
         // make sure user has required roles
-        for (let roleId of member.data.roles) {
-          for (let role of roles.data) {
-            if (ctx.session && role.id === roleId && config.discord.auth.roles.indexOf(role.name) !== -1) {
-              ctx.session.id = user.data.id;
-              ctx.session.username = user.data.username;
-              ctx.session.avatar = user.data.avatar;
-              ctx.session.discriminator = user.data.discriminator;
-              ctx.redirect('/');
-              return;
-            }
+        for (let role of roles) {
+          if (config.discord.auth.roles.split(' ').indexOf(role.name) !== -1) {
+            ctx.session.id = user.data.id;
+            ctx.session.username = user.data.username;
+            ctx.session.avatar = user.data.avatar;
+            ctx.session.discriminator = user.data.discriminator;
+            ctx.redirect('/');
+            return;
           }
         }
         ctx.throw(401, 'You are not in the guild');
@@ -108,26 +110,56 @@ export default class YuriKoa {
     });
 
     router.get('/', this.checkLogin, async (ctx: Koa.Context) => {
+      if (!ctx.session) {
+        ctx.throw(401);
+        return;
+      }
+
       let tasks = await this.opt.taskDao.getAllTasks();
       let scheduled = this.opt.scheduler.getSchedule();
       let channels = await this.opt.discordDao.listTextChannels(config.discord.auth.guildId);
       let guild = await this.opt.discordDao.getGuild(config.discord.auth.guildId);
       let members = guild.members;
 
+      let member = members.get(ctx.session.id);
+      if (!(member && (member.roles.exists('name', '姊姊大人') || member.roles.exists('name', '程式組')))) {
+        channels = channels.filter(c => c.name === '發佈機debug專用');
+      }
+
       await this.render(ctx, 'index', { tasks, scheduled, channels, members, session: ctx.session });
     });
 
     router.post('/tasks', this.checkLogin, async (ctx: Koa.Context) => {
+      if (!ctx.session) {
+        ctx.throw(401);
+        return;
+      }
       if (!ctx.request.body) {
         ctx.throw(400);
         return;
       }
+
       let { content, channel_id } = ctx.request.body as any;
       let guild = await this.opt.discordDao.getGuild(config.discord.auth.guildId);
       let channel = await this.opt.discordDao.getTextChannel(config.discord.auth.guildId, channel_id);
+
+      if (channel.name !== '發佈機debug專用') {
+        let roles = await this.getMemberRoles(ctx.session.id);
+        let allowed = false;
+        for (let role of roles) {
+          if (['姊姊大人', '程式組'].indexOf(role.name) > -1) {
+            allowed = true;
+          }
+        }
+        if (!allowed) {
+          ctx.throw(401);
+          return;
+        }
+      }
+
       await this.opt.taskDao.createTask({
         id: 0,
-        author: ctx.session ? ctx.session.id : '',
+        author: ctx.session.id,
         platform: 'discord',
         param: {
           content: content,
@@ -207,5 +239,22 @@ export default class YuriKoa {
 
   listen(...args: Array<any>): Server {
     return this.app.listen(...args);
+  }
+
+  private async getMemberRoles(memberId: string): Promise<Array<IRole>> {
+    const client = axios.create();
+    client.defaults.headers.common['Authorization'] = 'Bot ' + config.discord.bot.token;
+    let roles = await client.get(`https://discordapp.com/api/guilds/${config.discord.auth.guildId}/roles`);
+    let member = await client.get(`https://discordapp.com/api/guilds/${config.discord.auth.guildId}/members/${memberId}`);
+
+    let result = [];
+    for (let roleId of member.data.roles) {
+      for (let role of roles.data) {
+        if (role.id === roleId) {
+          result.push(role);
+        }
+      }
+    }
+    return result;
   }
 }
